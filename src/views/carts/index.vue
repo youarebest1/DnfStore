80f45e06190ae92f77dd6763b8ab3a137775a06d
Qi <junqi0423@foxmail.com>
2021-08-11 12:03:44 +0800

    12.03

--- src/views/carts/index.vue
+++ src/views/carts/index.vue
@@ -1,11 +1,20 @@
 <template>
-    <div class=''>购物车页面</div>
+    <div class=''>
+        <div id="header">
+            <a></a>
+        </div>
+        
+<Connav></Connav>
+    </div>
 </template>
 
 <script>
+import Connav from '../../components/con_nav_black.vue'
 export default {
     
-    components: {},
+    components: {
+        Connav
+    },
     data() {
         
         return {
@@ -27,5 +36,29 @@ export default {
     }
 </script>
 <style scoped>
+    #header{
+        width: 100vw;
+        height: 100px;
+        /* background-color: red; */
+        background-image:url( "https://img.dnfcity.qq.com/weixin20/base/cart_bg.png");
+         background-size:100vw;
+         text-align: center;
+         line-height: 100px;
+         font-size: 16px;
+         position: relative;
+    }
+    #header a{
+        display: block;
+        width: 77px;
+        height: 27px;
+        position: absolute;
+       background: url("https://img.dnfcity.qq.com/weixin20/base/cart_title.png") no-repeat; 
+       background-size: 100% 100%;
+       left: 40vw;
+       top:30px;
+    }
     
+   
+    
+   
 </style>
