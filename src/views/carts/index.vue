7f1605641ec57edee7e34ccdc050e5b070ed535b
Qi <junqi0423@foxmail.com>
2021-08-11 14:37:50 +0800

    旗

--- src/views/carts/index.vue
+++ src/views/carts/index.vue
@@ -1,15 +1,24 @@
 <template>
     <div class=''>
+        <!-- 购物车头部 -->
         <div id="header">
             <a></a>
         </div>
-        
+        <!-- 购物车没有数据 -->
+        <div id="nolist">
+            <div>
+                <router-link to="shouye"><div id="shouye"></div></router-link>
+                <img src="https://img.dnfcity.qq.com/weixin20/base/cart_no.png" alt="" width="220px" height="320px" >
+               
+            </div>
+        </div>
 <Connav></Connav>
     </div>
 </template>
 
 <script>
 import Connav from '../../components/con_nav_black.vue'
+import {get} from "../../util/request";
 export default {
     
     components: {
@@ -25,10 +34,13 @@ export default {
     watch: {},
     
     methods: {
-        
+       async CarList(){
+           const res = await get('/api/v1/shop_carts')
+           console.log(res);
+       }
     },
     created() {
-        
+        this.CarList()
     },
     mounted() {
         
@@ -54,10 +66,30 @@ export default {
         position: absolute;
        background: url("https://img.dnfcity.qq.com/weixin20/base/cart_title.png") no-repeat; 
        background-size: 100% 100%;
-       left: 40vw;
+       left: 45vw;
        top:30px;
     }
-    
+    #nolist{
+        
+        width: 100vw;
+        /* background: url("https://img.dnfcity.qq.com/weixin20/base/cart_no.png") no-repeat; */
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+    #nolist div{
+    position: relative;
+    }
+ 
+   
+    #shouye{
+        width: 152px;
+        height: 48px;
+        /* border: 1px solid black; */
+        position: absolute;
+        top: 312px;
+        left: 34px;
+    }
    
     
    
