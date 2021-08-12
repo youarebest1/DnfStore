4e5e2393c40695013cde21f51832cfab555de614
Lily <910133765@qq.com>
2021-08-12 15:51:46 +0800

    notdata

---
+++ src/components/notdata.vue
@@ -0,0 +1,62 @@
+<template>
+	<div class="wu">
+		<van-nav-bar :title="title" left-arrow @click-left="onClickLeft" />
+		<div class="cont">
+			<van-icon name="warning-o" />
+			<h3>勇士，暂无数据</h3>
+		</div>
+		<van-button type="default" class="bottom-button" to="shouye"
+			>周边首页</van-button
+		>
+	</div>
+</template>
+
+<script>
+export default {
+	props: {
+		title: {
+			type: String,
+			require: true,
+		},
+	},
+	components: {},
+	data() {
+		return {}
+	},
+	computed: {},
+	watch: {},
+
+	methods: {},
+	created() {},
+	mounted() {},
+}
+</script>
+<style scoped>
+.wu {
+	width: 100%;
+	height: 100%;
+	background-color: #f7f7f7;
+	display: block;
+}
+.cont {
+	margin-top: 50px;
+	color: #aaa;
+	text-align: center;
+}
+.van-icon-warning-o {
+	font-size: 30px;
+}
+.cont h3 {
+	font-weight: normal;
+	line-height: 40px;
+}
+.bottom-button {
+	width: 160px;
+	height: 40px;
+	border-radius: 5px;
+	position: absolute;
+	left: 0;
+	right: 0;
+	margin: auto;
+}
+</style>
--- src/views/my/myCDK.vue
+++ src/views/my/myCDK.vue
@@ -1,19 +1,35 @@
 <template>
-	<div class="">CDK</div>
+	<div class="box">
+		<Nodata title="我的CDK"></Nodata>
+	</div>
 </template>
 
 <script>
+import Nodata from '../../components/notdata.vue'
 export default {
-	components: {},
+	components: {
+		Nodata,
+	},
 	data() {
 		return {}
 	},
 	computed: {},
 	watch: {},
 
-	methods: {},
+	methods: {
+		onClickLeft() {
+			this.$router.push('/my')
+		},
+	},
 	created() {},
 	mounted() {},
 }
 </script>
-<style scoped></style>
+<style scoped>
+.box {
+	display: block;
+	background-color: #f7f7f7;
+	min-height: 568px;
+	overflow-y: hidden;
+}
+</style>
